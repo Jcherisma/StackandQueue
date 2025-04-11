1c6153bf2f07436dae20affedb93385356c21079
Jeffrey Cherisma <jeffreycherisma@Jeffreys-Air.attlocal.net>
2025-04-11 13:50:16 -0400

    added method to reverse strings using a stack and test to test that method, while adding a test folder

--- src/proj/Main.java
+++ src/proj/Main.java
@@ -3,10 +3,31 @@ package src.proj;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+       StringBuilder holder =  reverseString("Hello");
+
+        System.out.println(holder);
+
     }
 
     public String methodToTest() {
         // This is the method we want to test
         return "Expected Result";
+    }
+
+    //This method takes a string and reverses it using a stack
+    //we use a StrinBuilder rather than a String because String is immutable
+    //and StringBuilder is mutable
+    public static StringBuilder reverseString(String str) {
+        StringBuilder reversed = new StringBuilder();
+
+        Stack<Character> myStack = new Stack<>();
+        for (char c : str.toCharArray()) {
+            myStack.push(c);
+        }
+        while (!myStack.isEmpty()) {
+            reversed.append(myStack.pop());
+        }
+        return reversed;
     }
 }
